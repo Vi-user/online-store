fd7dfe426c0bc28f1d8617c551422481c0135428
Vi-user <vi.volkova.2712@gmail.com>
2022-12-16 00:02:21 +0300

    add app structure

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './pages/Layout';
+import Main from './pages/Main';
+import Basket from './pages/Basket';
+import Product from './pages/Product';
+import ErrorPage from './pages/Error';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="main" element={<Main />} />
+        <Route path="basket" element={<Basket />} />
+        <Route path="product/:id" element={<Product />} />
+        <Route path="error" element={<ErrorPage />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
