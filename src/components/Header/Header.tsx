9cbfe073a10393507b558eeceb4f88993753025d
Vi-user <vi.volkova.2712@gmail.com>
2022-12-24 19:36:04 +0300

    refactor: add basket sum & quantity to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Header.scss';
 import { Link } from 'react-router-dom';
+import { ProductsPriceInBasket, ProductsAmountInBasket } from '../../utils/helper';
+import { BasketContext } from '../../App';
+import { EURO_SYMBOL } from '../../utils/data';
 
 // interface HeaderProps {JSX: Element;}
 // const Header: FC<HeaderProps> = () => (
-const Header = () => (
-  <div className='header__wrapper'>
-    <header className='header'>
-      <Link className='logo' to='/'>
-        Online Store
-      </Link>
-      <div className='cart__total'>
-        Cart total:
-        <span>0</span>
-      </div>
-      <Link className='logo__basket' to='/basket'></Link>
-    </header>
-  </div>
-);
+const Header = () => {
+  const { basketState } = useContext(BasketContext);
+
+  return (
+    <div className='header__wrapper'>
+      <header className='header'>
+        <Link className='logo' to='/'>
+          Online Store
+        </Link>
+        <div className='cart__total'>
+          Cart total: {EURO_SYMBOL}{' '}
+          {new Intl.NumberFormat('ru-RU').format(ProductsPriceInBasket(basketState))}
+        </div>
+        <Link className='logo__basket' to='/basket'>
+          <span className='logo__basket-sum'>{ProductsAmountInBasket(basketState)}</span>
+        </Link>
+      </header>
+    </div>
+  );
+};
 
 export default Header;
