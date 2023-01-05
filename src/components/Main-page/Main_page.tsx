6be76514a44caf9ad2e86d08e5c0410eaad1866b
Oleg Voitko <pel6men6.oleg@gmail.com>
2023-01-05 18:24:53 +0300

    feat: add query-category & brand

--- src/components/Main-page/Main_page.tsx
+++ src/components/Main-page/Main_page.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import { EURO_SYMBOL, products } from '../../utils/data';
-import { handleFilterChangeBrand, handleFilterChangeCategory } from '../../utils/helper';
 import { Product } from '../../utils/types';
 import DualSlider from '../Dual-slider/Dual-slider';
 import FilterCategory from '../Filter-category/Filter-category';
 import ProductGrid from '../Product-grid/Product-grid';
 import './Main_page.scss';
+import { useSearchParams } from 'react-router-dom';
 
 const CATEGORIES: string[] = [];
 Array.from(new Set(products.map((el) => el.category))).map((category) => CATEGORIES.push(category));
@@ -27,13 +27,18 @@ const minStockProduct: Product = products.reduce((acc, curr) =>
 
 function Main_page(): JSX.Element {
   const [productCard] = useState(products);
-  const [filterCategory, setfilterCategory] = useState<string[]>([]);
-  const [filterBrand, setfilterBrand] = useState<string[]>([]);
+  // const [filterCategory, setfilterCategory] = useState<string[]>([]);
+  // const [filterBrand, setfilterBrand] = useState<string[]>([]);
   const [priceMin, setPriceMin] = useState(0);
   const [priceMax, setPriceMax] = useState(0);
   const [stockMin, setStockMin] = useState(0);
   const [stockMax, setStockMax] = useState(0);
   const [search, setSearch] = useState('');
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParamsCategory, setSearchParamsCategory] = useSearchParams();
+  const [searchParamsBrand, setSearchParamsBrand] = useSearchParams();
+  const filterCategory = searchParamsCategory.get('category')?.split(',') || [];
+  const filterBrand = searchParamsBrand.get('brand')?.split(',') || [];
 
   const filterCardByFiltered: Product[] = filterCategory.length
     ? productCard.filter((e) => filterCategory.includes(e.category))
@@ -64,6 +69,120 @@ function Main_page(): JSX.Element {
     );
   });
 
+  const [open, setOpen] = useState(false);
+  const [selected, setSelected] = useState(0);
+  const list = [
+    ' .....',
+    ' Sort by price ASC',
+    ' Sort by price DESK',
+    ' Sort by discount ASC',
+    ' Sort by discount DESC',
+  ];
+
+  const onClickListItem = (i: React.SetStateAction<number>) => {
+    setSelected(i);
+    setOpen(false);
+  };
+
+  const sortOptionsCard: Product[] = filtererdCardbySearch.filter(() => {
+    if (selected === 0) {
+      return productCard;
+    }
+    if (selected === 1) {
+      return productCard.sort((a, b) => a.price - b.price);
+    }
+    if (selected === 2) {
+      return productCard.sort((a, b) => b.price - a.price);
+    }
+    if (selected === 3) {
+      return productCard.sort((a, b) => a.discountPercentage - b.discountPercentage);
+    }
+    if (selected === 4) {
+      return productCard.sort((a, b) => b.discountPercentage - a.discountPercentage);
+    }
+  });
+
+  // const maxPriceProduct: Product = useMemo(() => filtererdCardbySearch.reduce((acc, curr) =>
+  // acc.price > curr.price ? acc : curr, {} as Product
+  // ), [filtererdCardbySearch])
+
+  // const minPriceProduct: Product = useMemo(() => filtererdCardbySearch.reduce((acc, curr) =>
+  // acc.price < curr.price ? acc : curr, {} as Product
+  // ), [filtererdCardbySearch])
+
+  // const maxStockProduct: Product = products.reduce((acc, curr) =>
+  //   acc.stock > curr.stock ? acc : curr
+  // );
+  // const minStockProduct: Product = products.reduce((acc, curr) =>
+  //   acc.stock < curr.stock ? acc : curr
+  // );
+
+  const onClickSmallGrid = () => {};
+
+  const setfilterCategory = (e: { target: { checked: boolean; value: string } }) => {
+    const checked = e.target.checked;
+    const value = e.target.value;
+    const currentParams = Object.fromEntries([...searchParamsCategory]);
+    if (checked) {
+      const newFilter = [...filterCategory, value].join(',');
+      setSearchParamsCategory({ ...currentParams, category: newFilter });
+    } else {
+      const indexValue = filterCategory.indexOf(value);
+      const newFilter = [
+        ...filterCategory.slice(0, indexValue),
+        ...filterCategory.slice(indexValue + 1),
+      ].join(',');
+      if (!newFilter.length) {
+        delete currentParams.category;
+        setSearchParamsCategory({
+          ...currentParams,
+        });
+      } else {
+        setSearchParamsCategory({ ...currentParams, category: newFilter });
+      }
+    }
+  };
+
+  const setfilterBrand = (e: { target: { checked: boolean; value: string } }) => {
+    const checked = e.target.checked;
+    const value = e.target.value;
+    const currentParams = Object.fromEntries([...searchParamsBrand]);
+    console.log(currentParams);
+    if (checked) {
+      const newFilter = [...filterBrand, value].join(',');
+      setSearchParamsBrand({ ...currentParams, brand: newFilter });
+    } else {
+      const indexValue = filterBrand.indexOf(value);
+      const newFilter = [
+        ...filterBrand.slice(0, indexValue),
+        ...filterBrand.slice(indexValue + 1),
+      ].join(',');
+      if (!newFilter.length) {
+        delete currentParams.brand;
+        setSearchParamsBrand({
+          ...currentParams,
+        });
+      } else {
+        setSearchParamsBrand({ ...currentParams, brand: newFilter });
+      }
+    }
+  };
+
+  const handleSubmit = (e: { target: any; preventDefault: () => void }) => {
+    e.preventDefault();
+    const form = e.target;
+    const query = form.search.value;
+    const currentParams = Object.fromEntries([...searchParams]);
+
+    setSearchParams({ ...currentParams, search: query });
+    if (!query.length) {
+      delete currentParams.search;
+      setSearchParams({
+        ...currentParams,
+      });
+    }
+  };
+
   return (
     <div className='main_page'>
       <div className='filters__panel'>
@@ -74,19 +193,13 @@ function Main_page(): JSX.Element {
         <div className='nav__category filters'>
           <div className='category__title'>Category</div>
           <div className='category__container'>
-            <FilterCategory
-              categories={CATEGORIES}
-              onFilterChange={(e) => handleFilterChangeCategory(e, setfilterCategory)}
-            />
+            <FilterCategory categories={CATEGORIES} onFilterChange={setfilterCategory} />
           </div>
         </div>
         <div className='nav__brand filters'>
           <div className='category__title'>Brand</div>
           <div className='category__container'>
-            <FilterCategory
-              categories={BRAND}
-              onFilterChange={(e) => handleFilterChangeBrand(e, setfilterBrand)}
-            />
+            <FilterCategory categories={BRAND} onFilterChange={setfilterBrand} />
           </div>
         </div>
         <div className='dual-slider'>
@@ -114,16 +227,40 @@ function Main_page(): JSX.Element {
       </div>
       <div className='card-product'>
         <div className='card-product__header'>
-          <button>Sort by discount</button>
+          {/* <Sort value={sortType} onClickCategory={(i: React.SetStateAction<number>) => setSortType(i)}/> */}
+
+          <div className='sort'>
+            <div className='sort__label'>
+              <b>Sort options: </b>
+              <span onClick={() => setOpen(!open)}>{list[selected]}</span>
+            </div>
+            {open && (
+              <div className='sort__popup'>
+                <ul>
+                  {list.map((name, i) => (
+                    <li
+                      key={i}
+                      onClick={() => onClickListItem(i)}
+                      className={selected === i ? 'active' : ''}
+                    >
+                      {name}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </div>
+
           <div className='found'>
             Found:
             <span>{filtererdCardbySearch.length}</span>
           </div>
           {/* <ProductsAmountCards productCard={state.productCard} /> */}
           <div className='form'>
-            <form className='search-product'>
+            <form className='search-product' onSubmit={handleSubmit}>
               <input
                 type='text'
+                name='search'
                 placeholder='Search product...'
                 className='search__input'
                 onChange={(event) => setSearch(event.target.value)}
@@ -131,12 +268,12 @@ function Main_page(): JSX.Element {
             </form>
           </div>
           <div className='button__product-grid'>
-            <button></button>
-            <button></button>
+            <button onClick={() => onClickSmallGrid()} className='smallGrid'></button>
+            <button className='bigGrid'></button>
           </div>
         </div>
         <ul className='product__grid'>
-          {filtererdCardbySearch.map((product) => (
+          {sortOptionsCard.map((product) => (
             <ProductGrid key={product.id} product={product} />
           ))}
         </ul>
