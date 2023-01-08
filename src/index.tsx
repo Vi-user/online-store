6a0832b8c9308a6b7425d62bf12b163b85f5a3fa
Oleg Voitko <pel6men6.oleg@gmail.com>
2023-01-08 14:18:15 +0300

    fix: fix browser roots for deploy project

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
